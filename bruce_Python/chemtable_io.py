f3f766af83c75c50e0f2a1eb60e5ebfa6375534c
Bruce Alan Perry <baperry@princeton.edu>
2014-12-05 10:46:13 -0500

    update python wrapper

--- bruce_Python/chemtable_io.py
+++ bruce_Python/chemtable_io.py
@@ -1,61 +1,48 @@
 #!/usr/bin/env python
 
 import numpy as np
+import iofuncs as iof
+import findprogvar as fpv
 
+# read input file
 fin1 = open('chemtable_inputs')
 inputs = [line.strip().split('\t') for line in fin1]
+datafiles = iof.read_input("data files:", inputs)
+testspecies = iof.read_input("test species:", inputs, 0)
+if testspecies == []: # add default value
+    testspecies = ["Y-CO2","Y-CO","Y-H2O"]
+    print "WARNING: no species specified, using Y-CO2, Y-CO, Y-H2O"
 
-filesflag = 0
-speciesflag = 0
-for kk in range(len(inputs)):
-    if inputs[kk][0] == "data files:":
-        if len(inputs[kk]) < 2:
-            raise IOError("must specify at least 1 datafile")
-        if filesflag == 1:
-            raise IOError("cannot specify data files on multiple lines")
-        datafiles = inputs[kk][1:len(inputs[kk])]
-        filesflag = 1
-        print datafiles #####
-    if inputs[kk][0] == "test species:":
-        if len(inputs[kk]) < 2:
-            raise IOError("must specify at least 1 species")
-        if speciesflag == 1:
-            raise IOError("cannot specify progress variable species on multiple lines")
-        testspecies = inputs[kk][1:len(inputs[kk])]
-        speciesflag = 1
-        print testspecies #####
-if filesflag == 0:
-    raise IOError("data files not specified in input file, syntax is <datafile:\t file1\t file2\t ...>")
-if speciesflag == 0: 
-    raise IOError("species to be tested in progress variable calculations not specified in input file, syntax is <testspecies:\t species1\t species2\t ...>") 
+# find best progress variable
+bestC = []
+nofiles = len(datafiles)
+filesmatrix = np.zeros((nofiles,2)) # define more generally
+fpv.findC(datafiles, testspecies, bestC, filesmatrix)
+print filesmatrix
+print bestC
 
-sfile = datafiles[0]
-inputvars = testspecies
+# sort FILESMATRIX by progress variable
+# will add connectivity to C++ sort later
+print "sorting FILESMATRIX by C"
 
-fin = open(sfile)
-l1 = fin.readline()
-l2 = fin.readline()
-titles = l2.strip().split('\t')
-fin.close()
+# Calculate PDF matrix
+Zvar_max = iof.read_input("Zvar_max:", inputs)
+Zvar_grid = iof.read_input("Zvar_grid:", inputs)
+Zpdf = iof.read_input("Zvar_grid:", inputs)
 
-locations = range(len(inputvars)+2)
-print locations #####
-jj=2
-for inputvar in inputvars:
-    ii = 0
-    flag = 0
-    for title in titles:
-        if title == inputvar:
-            locations[jj] = ii
-            flag = 1
-        ii = ii+1
-    if flag == 0:
-        raise IOError("user specified column header %s not found in specified data file %s" % (inputvar, sfile))
-    jj = jj+1
-print locations ####
+Zvar = np.linspace(0, float(Zvar_max[0]), int(Zvar_grid[0]))
+Zmean = np.genfromtxt(datafiles[0], unpack=False, skiprows=2, delimiter = "\t", usecols = 0)
+# call C++ function to generate PDFmatrix (inputs: Zvar, Zmean, Zpdf)
 
-data = np.genfromtxt(sfile, unpack=False, skiprows=2, delimiter = "\t", usecols = locations)
+# obtain relevant Yi and reaction rates from each file
+for kk in filesmatrix[:,1]:
+    file = datafiles[int(kk)]
+    # data will be extracted from this file and sent to the C++ function Convolute
 
-print data #####
+# Then, the C++ function FitToGrid will be called to generate the final output data
+# Contour plots will be used to visualize this data
 
-# test to see if all input data files are the same
+# Future functionality
+    # command line changes to arguments
+    # make possibility of default settings general
+
