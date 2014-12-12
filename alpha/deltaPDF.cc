5246492a3db38d3b626d0b315f90477a33a52a1c
Emmet Cleary <emcleary@princeton.edu>
2014-12-12 01:12:43 -0500

    modified deltaPDF.cc

--- alpha/deltaPDF.cc
+++ alpha/deltaPDF.cc
@@ -20,14 +20,6 @@ int DeltaPDF::pdfVal(Vector *Var, Vector *Mean, Matrix3D *pdfValM) {
   int ZPts     = pdfValM->GetNumDim3();
   assert(ZvarPts == 1);
 
-  /*
-  printf("\n");
-  printf("Zmean vector:\n");
-  for (int k = 0; k < ZmeanPts; k++) {
-    printf("Zmean[%d] = %f\n", k, Mean->GetVal(k));
-  }
-  */
-
   double Zmean, Zvar;
   double *temp = new double[ZmeanPts];
   double *Z = new double[ZmeanPts];
@@ -44,20 +36,25 @@ int DeltaPDF::pdfVal(Vector *Var, Vector *Mean, Matrix3D *pdfValM) {
       temp[k] = 0;
     }
 
-    // finds location of Zmean in Z
-    i = 0;
-    while (Z[i] < Zmean) {
-      i = i+1;
-    }
-
-    // calculates PDF
-    if (i == 0) {
+    if (Zmean == 1) {
+      temp[ZPts-1] = 1;
+    } else if (Zmean == 0) {
       temp[0] = 1;
     } else {
-      temp[i-1] = (Z[i]  - Zmean)  / (Z[i] - Z[i-1]);
-      temp[i]   = (Zmean - Z[i-1]) / (Z[i] - Z[i-1]);
-    }
+      // finds location of Zmean in Z
+      i = 0;
+      while (Z[i] < Zmean) {
+	i = i+1;
+      }
 
+      // calculates PDF
+      if (i == 0) {
+	temp[0] = 1;
+      } else {
+	temp[i-1] = (Z[i]  - Zmean)  / (Z[i] - Z[i-1]);
+	temp[i]   = (Zmean - Z[i-1]) / (Z[i] - Z[i-1]);
+      }
+    }
     // outputs PDF values into PDFValM
     for (int k = 0; k < ZPts; k++) {
       pdfValM->SetVal(0, m, k, temp[k]);
