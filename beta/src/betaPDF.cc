2155136eaefb73c868b8acef2d7d18f3954bfca9
Emmet Cleary <emcleary@princeton.edu>
2015-01-07 10:53:18 -0500

    betaPDF faster

--- beta/src/betaPDF.cc
+++ beta/src/betaPDF.cc
@@ -22,14 +22,7 @@ int BetaPDF::pdfVal(const double *Z, const int ZPoints, Matrix3D *pdfValM) {
   double *temp = new double[ZPoints];
   double ZvarVal, ZmeanVal, Weight;
   double alpha, beta, factor;
-  double dz, lnpdf, pdf1, pdf2, f;
-  double sum;
-
-  // For refining Z to reduce errors
-  int ModNum = 10001;
-  double *LowMod = new double[ModNum];
-  double *UppMod = new double[ModNum];
-
+  double lnpdf;
 
   int i;
   for (int n = 0; n < ZvarPoints_; n++) {
@@ -68,12 +61,6 @@ int BetaPDF::pdfVal(const double *Z, const int ZPoints, Matrix3D *pdfValM) {
 	/// BetaPDF
       } else {
 
-	// fill LowMod and UppMod
-	for (int k = 0; k < ModNum; k++) {
-	  LowMod[k] = double(k) / double(ModNum) / ZPoints;
-	  UppMod[ModNum-1-k] = Z[ZPoints-1] - LowMod[k];
-	}
-
 	alpha = ZmeanVal * (ZmeanVal * (1 - ZmeanVal) / ZvarVal - 1);
 	beta = alpha / ZmeanVal - alpha;
 	factor = lgamma(alpha + beta) - lgamma(alpha) - lgamma(beta);
@@ -83,36 +70,13 @@ int BetaPDF::pdfVal(const double *Z, const int ZPoints, Matrix3D *pdfValM) {
 	  lnpdf = factor + (alpha - 1) * log(Z[n]) + (beta - 1) * log(1 - Z[n]);
 	  temp[n] = exp(lnpdf);
 	}
-	
-	/// Calculate integral at ends
-	sum = 0;
-	dz = LowMod[1] - LowMod[0];
-	for (int k = 1; k < ModNum-1; k++) {
-	  pdf1 = exp(factor + (alpha - 1) * log(LowMod[k]) + (beta - 1) * log(1 - LowMod[k]));
-	  pdf2 = exp(factor + (alpha - 1) * log(LowMod[k+1]) + (beta - 1) * log(1 - LowMod[k+1]));
-	  f = 0.5 * (pdf1 + pdf2);
-	  sum = sum + f * dz;
-	  if (k == 1) {
-	    temp[0] = pdf1;
-	  }
 
-	  pdf1 = exp(factor + (alpha - 1) * log(UppMod[ModNum-k-2]) + (beta - 1) * log(1 - UppMod[ModNum-k-2]));
-	  pdf2 = exp(factor + (alpha - 1) * log(UppMod[ModNum-k-1]) + (beta - 1) * log(1 - UppMod[ModNum-k-1]));
-	  f = 0.5 * (pdf1 + pdf2);
-	  sum = sum + f * dz;
-	  if (k == ModNum-2) {
-	    temp[ZPoints-1] = pdf1;
-	  }
-	}
-
-	for (int n = 1; n < ZPoints-1; n++) {
-	  f = 0.5 * (temp[n+1] + temp[n]);
-	  dz = Z[n+1] - Z[n];
-	  sum = sum + f * dz;
-	}
+	// End points
+	temp[0] = temp[1];
+	temp[ZPoints-1] = temp[ZPoints-2];
 
 	for (int k = 0; k < ZPoints; k++) {
-	  temp[k] = temp[k]  / sum;
+	  temp[k] = temp[k];
 	}
       }
 
@@ -124,7 +88,5 @@ int BetaPDF::pdfVal(const double *Z, const int ZPoints, Matrix3D *pdfValM) {
   }
 
   delete temp;
-  delete LowMod;
-  delete UppMod;
   return 0;
 }
