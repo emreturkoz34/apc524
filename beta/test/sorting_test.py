878a159e635257ad270be7061f933eebbad49122
emre <eturkoz@princeton.edy>
2015-01-07 11:48:25 -0500

    added sorting_test.py

--- beta/test/sorting_test.py
+++ beta/test/sorting_test.py
@@ -9,21 +9,57 @@ sys.path.append('../mod')
 import sorting
 import matrix
 
+
+
+length = 15
+n = matrix.Matrix(length,2)
+for i in range(length):
+    n.SetVal(i,0,-i*(i-length-1))
+for i in range(length):
+    n.SetVal(i,1,i)
+
+
+def non_decreasing(array):
+    return all(x<=y for x, y in zip(array, array[1:]))
+
 class Sorting(unittest.TestCase):
+
 
     def testBubbleSort(self):
         print "\ntest Bubble sort simple case"
-        filesmatC = []
-        filesmatC.append([10, 20, 30, 40, 30, 20, 10])
-        filesmatC.append([1, 2, 3, 4, 5, 6, 7])
-        sorter = sorting.bubble_sort(filesmatC)
+        m = n
+        sorter = sorting.bubble_sort(m)
         sorter.SetRefColNum(0)
-        sorter.SetSortEndIndex(7)
-        sorter.generateIndexArray()
-        sorter.extractRefCol()
         sorter.sort_data()
+        arr = []
+        for i in range(length):
+            arr.append(m.GetVal(i,0))
+        self.assertTrue(non_decreasing(arr))
+
+        
+    def testStandardSort(self):
+        print "\ntest Standard sort simple case"
+        m = n
+        sorter = sorting.standard_sort(m)
+        sorter.SetRefColNum(0)
+        sorter.sort_data()
+        arr = []
+        for i in range(length):
+            arr.append(m.GetVal(i,0))
+        self.assertTrue(non_decreasing(arr))
+
         
 
+    def testBruteSort(self):
+        print "\ntest Brute sort simple case"
+        m = n
+        sorter = sorting.brute_sort(m)
+        sorter.SetRefColNum(0)
+        sorter.sort_data()
+        arr = []
+        for i in range(length):
+            arr.append(m.GetVal(i,0))
+        self.assertTrue(non_decreasing(arr))
 
 
 if __name__ == '__main__':
