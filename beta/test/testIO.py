89c0471da4f559b9229695ab91cfd3acd7972b62
Bruce Perry <baperry@princeton.edu>
2015-01-07 14:41:05 -0500

    added more IO tests

--- beta/test/testIO.py
+++ beta/test/testIO.py
@@ -2,9 +2,12 @@
 
 import sys
 sys.path.append('../python')
+sys.path.append('../mod')
 
+import iofuncs as iof
 import combinations as cs
 import numpy as np
+import os
 import unittest
 
 
@@ -41,9 +44,41 @@ class TestFunctions(unittest.TestCase):
         for i in range(vectorlength):
             correctsum += cs.numcoms(12,i+1)*(i+1)
         self.assertEqual(correctsum, testmatrix.sum())
+    
+    # test the gettitles function in ioFuncs
+    def testGetTitles(self):
+        f = open('testfile','w')
+        f.write('* \n')
+        strings = ['dojkfr', 'jkn   vsnkl', 'afkjaf', 'ajknnjv   nsv', '99"|} {{{}}#^&$']
+        for i in strings:
+            f.write(i)
+            f.write('\t')
+        f.close()
+        dataobj = iof.ProcFile('testfile')
+        titles = dataobj.gettitles()
+        os.remove('testfile')
+        np.testing.assert_array_equal(strings,titles)
+
+    #test the read_inpit function in ioFuncs
+    def testReadInput(self):
+        inputsarray = [0] * 4
+        inputsarray[0] = ['input1:', 3]
+        inputsarray[1] = ['input2:', 'dogs']
+        inputsarray[2] = ['input3:', 'rats']
+        inputsarray[3] = ['input3:', 'cats']
+        self.assertEqual(['dogs'], iof.read_input('input2:',inputsarray))
+        self.assertEqual([3], iof.read_input('input1:', inputsarray))
+        self.assertEqual([3], iof.read_input('input17:', inputsarray, default=[3]))
+        self.assertEqual([3], iof.read_input('input1:', inputsarray, default=[2]))
+        self.assertRaises(IOError, iof.read_input,'input3:',inputsarray)
+        self.assertRaises(IOError, iof.read_input,'input17:',inputsarray)
+        self.assertRaises(IOError, iof.read_input,'input1',inputsarray)
+        self.assertRaises(IOError, iof.read_input,'input2:',inputsarray, minargs=2)
+        
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
 
