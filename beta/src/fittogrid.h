6d5b84a9b6ced62a9f683c1073938a1f091fff8f
Daniel Floryan <dfloryan@princeton.edu>
2015-01-11 14:41:48 -0500

    Fixed fittogrid header file

--- beta/src/fittogrid.h
+++ beta/src/fittogrid.h
@@ -5,7 +5,7 @@
 #include "matrix3d.h"
 #include "matrix4d.h"
 #include "interpolator.h"
-#include "sorting.h"
+#include "standard_sort.h"
 #include <omp.h>
 
 int fittogrid(const Matrix4D *datain, const double *cgrid, Interpolator *interp, Matrix3D *dataout, int nthreads);
